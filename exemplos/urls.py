f957b3d737a99d01d79fd228becb61d168361b77
jadirrlima@gmail.com <@Wilson20@>
2022-11-27 17:18:46 -0400

    Exemplos Bootstrap

---
+++ exemplos/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.get_bootstrap, name='bootstrap')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# path('processa_formulario_v1', views.processa_formulario_v1, name="processa_formulario_v1"),
+# path('processa_formulario_v2', views.processa_formulario_v2, name="processa_formulario_v2"),
